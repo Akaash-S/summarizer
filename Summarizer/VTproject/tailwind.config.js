070afaa2d41319cecfdf95bc22cf58651055cc68
Dharun Kumar  S H <freebirdprod.official@gmail.com>
2025-06-25 15:07:21 +0530

    h

--- Summarizer/VTproject/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/* eslint-env node */
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
--- vt-summarize/src/auth/login.jsx
+++ vt-summarize/src/auth/login.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 function LoginPage() {
-  return (
-    <div>LoginPage</div>
-  )
+    return (
+        <div className='w-full h-screen flex items-start'>
+            <div className='relative w-1/2 h-full flex flex-col'>
+                <div className='absolute top-[25%] left-[10%] flex flex-col'>
+                    <h1>Speak Freely. Summarize Smartly.</h1>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default LoginPage
