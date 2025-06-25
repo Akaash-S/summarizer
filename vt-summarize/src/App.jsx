2da1191ba6b6c7625303a3c6cf8cff9ce6b54b71
Dharun Kumar  S H <freebirdprod.official@gmail.com>
2025-06-25 15:02:53 +0530

    f

--- vt-summarize/src/App.jsx
+++ vt-summarize/src/App.jsx
@@ -1,8 +1,14 @@
 import './App.css'
+import LoginPage from './auth/login'
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+      </Routes>
+
     </>
   )
 }
